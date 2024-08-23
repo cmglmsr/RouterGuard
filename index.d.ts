689e93e384cfb9b35e955ef60c633455c4a4503a
Cem Gülümser <mgulumser@my.aci.k12.tr>
2024-08-23 14:21:32 +0300

    1.0.5

---
+++ index.d.ts
@@ -0,0 +1,34 @@
+declare module '/data/index.js' {
+    export const payloads: Array<[string, RegExp[]]>;
+}
+
+declare module '/utils/bodyUtils.js' {
+    export function traverse(obj: object | string, regex: RegExp): boolean;
+}
+
+declare class rtguard {
+    plevel: number;
+    allowedBodyTypes: string[];
+    allowedMethods: string[];
+    maxRequestSize: number;
+    verbose: boolean;
+
+    constructor(config?: {
+        plevel?: number;
+        allowedBodyTypes?: string[];
+        allowedMethods?: string[];
+        maxRequestSize?: number;
+        verbose?: boolean;
+    });
+
+    initialAudit(req: { method: string; headers: { [key: string]: string } }): string | false;
+    checkURL(url: string, regex: RegExp): boolean;
+    checkHeaders(headers: object, regex: RegExp): boolean;
+    checkJsonBody(body: object, regex: RegExp): string | boolean;
+    rtguard(req: object, res: object, next: Function): void;
+    logSummary(audits: object[], start: [number, number], blocked: boolean, req: object): void;
+    auditSummary(audits: object[]): string;
+    log(args: any[]): void;
+}
+
+export { rtguard };
